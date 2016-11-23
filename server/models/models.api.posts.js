91799fe4ea58886c6cf5b6f930975dc906e86723
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-23 23:42:26 +0700

    create post model

--- server/models/models.api.posts.js
+++ server/models/models.api.posts.js
@@ -1,3 +1,34 @@
-/**
- * Created by dharmadi93 on 11/23/16.
- */
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Post = new Schema({
+    createdBy: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    postId: {
+        type: Number
+    },
+    comments: [
+        {
+            createdBy: {
+                type: Schema.ObjectId,
+                ref: 'User'
+            },
+            content: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+}, {
+    timestamps: true
+})
+
+Question.pre('find', function (next) {
+    this.populate('createdBy', 'username')
+    this.populate('comments.$.createdBy', 'username')
+    next()
+})
+
+module.exports = mongoose.model('Question', Question)
